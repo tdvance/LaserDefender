737d749817a1947f715e80a1c968daea7da663c8
Todd D. Vance <tdvance@gmail.com>
2016-08-17 10:54:09 -0400

    Starting of a menu system

---
+++ Assets/Menu/BlueScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BlueScreen : GenericMenu
+{
+    public string errorMessage = "Stub: error message goes here";
+    new void Start()
+    {
+        base.Start();
+        subtitle.text = errorMessage;
+    }
+    void Update()
+    {
+        // do nothing
+    }
+}
---
+++ Assets/Menu/GenericMenu.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GenericMenu : MonoBehaviour {
+    public float screenDuration = 10f;
+    public String nextScreen = "BlueScreen";
+    public String gameScreen = "BlueScreen";
+
+    public Text title;
+    public Text subtitle;
+
+    // Use this for initialization
+    protected void Start () {
+        if (screenDuration > 0)
+        {
+            Invoke("NextScreen", screenDuration);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkKeyPresses();
+        checkButtonClicks();
+	}
+
+    private void checkKeyPresses()
+    {
+        if (Input.anyKeyDown)
+        {
+            StartGame();
+        }
+    }
+
+    private void StartGame()
+    {
+        Debug.Log("Start Game: " + gameScreen);
+        SceneManager.LoadScene(gameScreen);
+
+    }
+
+    private void checkButtonClicks()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            StartGame();
+        }
+    }
+
+    void NextScreen()
+    {
+        Debug.Log("Next Screen: " + nextScreen);
+        SceneManager.LoadScene(nextScreen);
+    }
+}
